46d0ceca26cb4dfb13364e51ad2f421845b8a55f
Mason Barnes <barnesmason980@gmail.com>
2024-05-01 13:10:34 -0700

    lab Complete

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+
+
+const names = ["Guadalupe", "Ollie", "Aki"]
+
+
+function writeCards(names){
+    
+    let cards = [];
+    for (let i = 0; i < names.length; i++){
+        cards.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    
+    }
+    return cards
+
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"]);
+
+
+function countDown(number){
+    let count = number
+    while(count >= 0){
+        console.log(count)
+       count = (count - 1);
+
+
+    }
+}
+
+countDown(11)
